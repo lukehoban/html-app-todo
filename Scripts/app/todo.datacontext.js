566f61890f503e9b96b946578563ceec6b0b1c56
Luke Hoban <lukeh@microsoft.com>
2013-05-01 14:52:13 -0700

    Initial commit

---
+++ Scripts/app/todo.datacontext.js
@@ -0,0 +1,91 @@
+﻿window.todoApp = window.todoApp || {};
+
+window.todoApp.datacontext = (function () {
+
+    var client = new WindowsAzure.MobileServiceClient("https://spa-todo.azure-mobile.net/", "sOpDvYyeKIPNTBwJHbAAyBeXVdaExT94");
+
+    var datacontext = {
+        client: client,
+        getTodoLists: getTodoLists,
+        createTodoItem: createTodoItem,
+        createTodoList: createTodoList,
+        saveNewTodoItem: saveNewTodoItem,
+        saveNewTodoList: saveNewTodoList,
+        saveChangedTodoItem: saveChangedTodoItem,
+        saveChangedTodoList: saveChangedTodoList,
+        deleteTodoItem: deleteTodoItem,
+        deleteTodoList: deleteTodoList
+    };
+
+    return datacontext;
+
+    function getTodoLists(todoListsObservable, errorObservable) {
+        var table = client.getTable("TodoList");
+        return table.read().then(function (data) {
+            var mappedTodoLists = $.map(data, function (list) { return new createTodoList(list); });
+            todoListsObservable(mappedTodoLists);
+        }, function (err) {
+            errorObservable("Error retrieving todo lists.");
+        });
+    }
+    function createTodoItem(data) {
+        return new datacontext.todoItem(data); // todoItem is injected by todo.model.js
+    }
+    function createTodoList(data) {
+        return new datacontext.todoList(data); // todoList is injected by todo.model.js
+    }
+    function saveNewTodoItem(todoItem) {
+        clearErrorMessage(todoItem);
+
+        var table = client.getTable("Todo");
+        return table.insert(todoItem.toJS()).then(function (result) {
+            todoItem.id = result.id;
+        }, function () {
+            todoItem.errorMessage("Error adding a new todo item.");
+        });
+    }
+    function saveNewTodoList(todoList) {
+        clearErrorMessage(todoList);
+
+        var table = client.getTable("TodoList");
+        return table.insert(todoList.toJS()).then(function (result) {
+            todoList.todoListId = result.todoListId;
+            todoList.userId = result.userId;
+        }, function () {
+            todoList.errorMessage("Error adding a new todo list.");
+        });
+    }
+    function deleteTodoItem(todoItem) {
+        var table = client.getTable("Todo");
+        return table.del(todoItem.toJS()).then(null, function () {
+            todoItem.errorMessage("Error removing todo item.");
+        });
+    }
+    function deleteTodoList(todoList) {
+        var table = client.getTable("TodoList");
+        return table.del(todoList.toJS()).then(null, function (e) {
+            todoList.errorMessage("Error removing todo list.");
+            throw e;
+        });
+    }
+    function saveChangedTodoItem(todoItem) {
+        clearErrorMessage(todoItem);
+
+        var table = client.getTable("Todo");
+        return table.update(todoItem.toJS()).then(null, function (e) {
+            todoItem.errorMessage("Error updating todo item.");
+        });
+    }
+    function saveChangedTodoList(todoList) {
+        clearErrorMessage(todoList);
+
+        var table = client.getTable("TodoList");
+        return table.update(todoList.toJS()).then(null, function (e) {
+            todoList.errorMessage("Error updating the todo list title. Please make sure it is non-empty.");
+        });
+    }
+
+    // Private
+    function clearErrorMessage(entity) { entity.errorMessage(null); }
+
+})();
---
+++ Scripts/app/todo.viewmodel.js
@@ -0,0 +1,61 @@
+﻿window.todoApp.todoListViewModel = (function (ko, datacontext) {
+    /// <field name="todoLists" value="[new datacontext.todoList()]"></field>
+    var todoLists = ko.observableArray(),
+        error = ko.observable(),
+        user = ko.observable(),
+        logIn = function (account) {
+            datacontext.client.login(account || "facebook")
+                .then(logInSucceeded, logInFailed);
+
+            function logInSucceeded(result) {
+                user(result);
+                datacontext.getTodoLists(todoLists, error); // load todoLists
+            }
+            function logInFailed(err) {
+                error(err);
+            }
+        },
+        logOff = function () {
+            datacontext.client.logout();
+            user(datacontext.client.currentUser);
+        },
+        addTodoList = function () {
+            var todoList = datacontext.createTodoList();
+            todoList.isEditingListTitle(true);
+            datacontext.saveNewTodoList(todoList)
+                .then(addSucceeded, addFailed);
+
+            function addSucceeded() {
+                showTodoList(todoList);
+            }
+            function addFailed() {
+                error("Save of new todoList failed");
+            }
+        },
+        showTodoList = function (todoList) {
+            todoLists.unshift(todoList); // Insert new todoList at the front
+        },
+        deleteTodoList = function (todoList) {
+            todoLists.remove(todoList);
+            datacontext.deleteTodoList(todoList)
+                .then(null, deleteFailed);
+
+            function deleteFailed() {
+                showTodoList(todoList); // re-show the restored list
+            }
+        };
+
+    return {
+        todoLists: todoLists,
+        error: error,
+        addTodoList: addTodoList,
+        deleteTodoList: deleteTodoList,
+        user: user,
+        logIn: logIn,
+        logOff: logOff
+    };
+
+})(ko, todoApp.datacontext);
+
+// Initiate the Knockout bindings
+ko.applyBindings(window.todoApp.todoListViewModel);
